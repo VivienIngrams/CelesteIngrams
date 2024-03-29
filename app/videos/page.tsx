083358693a118a9c82ccefe3d489ff2836d0f5c8
VivienIngrams <vivingrams@gmail.com>
2024-03-29 13:43:27 +0000

    DUmmy pages

---
+++ app/videos/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+const Practice = () => {
+  return (
+    <main id="Practice" className="w-full p-4 md:p-8 xl:p-20 min-h-screen">
+      <div className="w-full flex justify-center">
+        <h1 className="text-3xl lg:text-4xl p-2 md:p-6 font-light">Practice</h1>
+      </div>
+      
+    
+    </main>
+  );
+};
+export default Practice;
