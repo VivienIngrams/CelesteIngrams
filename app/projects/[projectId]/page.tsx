2accabfdad0da5f753f14919388807160d4c3c1c
VivienIngrams <vivingrams@gmail.com>
2024-03-29 19:11:36 +0000

    Starting projectId pages

--- app/projects/[projectId]/page.tsx
+++ app/projects/[projectId]/page.tsx
@@ -1,17 +1,44 @@
 "use client";
 
 import Image from "next/image";
+import { projects } from "../../data";
 import Link from "next/link";
 
-const Practice = () => {
+const Project = ({ params }: { params: { projectId: string } }) => {
+  const currentIndex = projects.findIndex(
+    (project) => project.id === params.projectId
+  );
+
+  // If the product is not found, you can handle it accordingly
+  if (currentIndex === -1) {
+    return <p>Product not found</p>;
+  }
+
+  const { id, title, text, images } = projects[currentIndex];
+
+  // Calculate indices for previous and next products
+  const prevIndex =
+    currentIndex > 0 ? currentIndex - 1 : projects.length - 1;
+  const nextIndex =
+    currentIndex < projects.length - 1 ? currentIndex + 1 : 0;
+
   return (
-    <main id="Practice" className="w-full p-4 md:p-8 xl:p-20 min-h-screen">
+    <main id={id} className="w-full p-4 md:p-8 xl:p-20 min-h-screen">
       <div className="w-full flex justify-center">
-        <h1 className="text-3xl lg:text-4xl p-2 md:p-6 font-light">Practice</h1>
+        <h1 className="text-3xl lg:text-4xl p-2 md:p-6 font-light">{title}</h1>
+        {/* <h2>{subtitle}</h2> */}
+        <p>{text}</p>
+        <div>
+          <Image 
+          src={images[0]}
+          alt={title}
+          width={500}
+          height={500}/>
+        </div>
       </div>
       
     
     </main>
   );
 };
-export default Practice;
+export default Project;
