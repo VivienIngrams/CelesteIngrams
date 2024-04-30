343e5a1f28c94e1317da6fdd2b2ddfca351bf357
VivienIngrams <vivingrams@gmail.com>
2024-04-30 17:45:26 +0100

    Changed env variables

--- client.js
+++ client.js
@@ -1,9 +1,9 @@
 import { createClient } from 'next-sanity'
 
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID 
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET 
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05-03' 
+const projectId = process.env.SANITY_PROJECT_ID 
+const dataset = process.env.SANITY_DATASET 
+const apiVersion = process.env.SANITY_API_VERSION || '2023-05-03' 
 
 export const client = createClient({
   projectId,
