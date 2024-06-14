cbd449664f2bf557c97e1678272f8fb4c81e1f12
VivienIngrams <vivingrams@gmail.com>
2024-06-14 11:29:54 +0100

    Space for works drawings

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -8,9 +8,9 @@ const Works = () => {
     <div className="w-full flex justify-center">
       <h1 className="pt-24 md:pt-40 lg:pt-12 text-3xl lg:text-4xl p-2 md:p-6 font-light text-center tracking-tighter">Works on Paper/Fabric</h1>
     </div>
-    <div className="w-full mx-auto flex flex-col justify-center items-center xs:w-[90vw] md:p-8 lg:pt-20 xl:pt-20 xl:px-20 md:grid lg:grid-cols-2 ">
+    <div className="w-full mx-auto flex flex-col justify-center items-center xs:w-[90vw] md:p-8 lg:pt-20 xl:pt-20 xl:px-10 md:grid lg:grid-cols-2 ">
         {drawings.map((drawing) => (
-          <div key={drawing.id} className="relative my-4 flex flex-col h-[250px] w-[250px] xs:w-[350px] xs:h-[400px] md:w-[600px] md:h-[600px] lg:w-[400px] lg:h-[240px] 2xl:w-[600px] 2xl:h-[430px] ">
+          <div key={drawing.id} className="relative my-4 flex flex-col h-[250px] w-[250px] xs:w-[350px] xs:h-[400px] md:w-[600px] md:h-[600px] lg:w-[400px] lg:h-[240px] 2xl:w-[580px] 2xl:h-[430px] ">
             <Image src={drawing.src} alt={drawing.alt} fill sizes="40vw" className="object-contain" />
                   
           </div>
