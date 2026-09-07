38e4b077156ec8faa3a641f8b72abef2933dec98
Vivien <vivingrams@gmail.com>
2026-09-07 18:56:54 +0100

    Links on page with images

--- app/collaborations/page.tsx
+++ app/collaborations/page.tsx
@@ -7,7 +7,7 @@ import {client} from "../../client";
 interface ProjectsType {
   id: string;
   title: string;
-  images: string;
+  images: string[];
 }
 
 // const collabProjects = projects.filter((project) => project.collaboration === true);
@@ -32,23 +32,25 @@ const Collaborations: React.FC = async () => {
       </div>
       <div className="w-full pt-8 mx-auto flex flex-col justify-center items-start xs:w-[90vw] md:p-8 lg:pt-20 xl:pt-20 xl:px-20 md:grid md:grid-cols-2 xl:grid-cols-3 gap-8">
         {collabProjects.map((project) => (
-          <Link href={`/projects/${project.id}`} key={project.id} className="mx-auto ">
-           
-            <div className="relative h-[200px] w-[200px] xs:w-[300px] xs:h-[300px] 2xl:w-[350px] 2xl:h-[350px] ">
-              <Image
-                src={project.images[0]}
-                alt={project.title}
-                fill
-                sizes="40vw"
-                className="object-cover"
-              />
-            </div>
-              <div className="mt-1 w-[200px] xs:w-[300px] 2xl:w-[350px]">
-                <h2 className=" text-xs xs:text-sm md:text-base font-light">
-                  {project.title}
-                </h2>
+          project.images && project.images.length > 0 && (
+            <Link href={`/projects/${project.id}`} key={project.id} className="mx-auto ">
+             
+              <div className="relative h-[200px] w-[200px] xs:w-[300px] xs:h-[300px] 2xl:w-[350px] 2xl:h-[350px] ">
+                <Image
+                  src={project.images[0]}
+                  alt={project.title}
+                  fill
+                  sizes="40vw"
+                  className="object-cover"
+                />
               </div>
-          </Link>
+                <div className="mt-1 w-[200px] xs:w-[300px] 2xl:w-[350px]">
+                  <h2 className=" text-xs xs:text-sm md:text-base font-light">
+                    {project.title}
+                  </h2>
+                </div>
+            </Link>
+          )
         ))}
       </div>
     </main>
